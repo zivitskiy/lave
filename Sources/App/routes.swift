9fa5c0097b5798c40974eabd6e591d51cb3f423f
dev <admin@MacBook-Pro-admin.local>
2024-08-23 12:02:25 +0300

    Initial

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,13 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get { req -> EventLoopFuture<Response> in
+        let filepath = req.application.directory.publicDirectory + "index.html"
+        return req.fileio.collectFile(at: filepath).map { file in
+            var headers = HTTPHeaders()
+            headers.add(name: .contentType, value: "text/html; charset=utf-8")
+            let body = Response.Body(buffer: file)
+            return Response(status: .ok, headers: headers, body: body)
+        }
     }
 }
